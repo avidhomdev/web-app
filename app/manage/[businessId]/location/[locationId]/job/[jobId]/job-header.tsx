cfde9b6f1f98f44671e0abf9dbbf559c714e7099
tony <dev@avid-hom.com>
2025-03-21 07:26:46 -0600

    Remove job header dropdown for status

--- app/manage/[businessId]/location/[locationId]/job/[jobId]/job-header.tsx
+++ app/manage/[businessId]/location/[locationId]/job/[jobId]/job-header.tsx
@@ -1,35 +1,19 @@
 "use client";
 
 import { IJob } from "@/types/job";
-import { Database } from "@/types/supabase";
-import { createClient } from "@/utils/supabase/client";
-import { Breadcrumb, Dropdown } from "flowbite-react";
+import { Breadcrumb } from "flowbite-react";
 import { ChevronLeftIcon, HardHatIcon } from "lucide-react";
-import { useParams, usePathname, useRouter } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 
 import PageTabs from "@/components/page-tabs";
-import { LOCATION_JOB_STATUS } from "@/constants/location-job-status";
 import { useBusinessContext } from "@/contexts/business";
 
 export default function JobHeader({ job }: { job: IJob }) {
-  const supabase = createClient();
-  const router = useRouter();
   const pathname = usePathname();
   const { businessId, locationId, jobId } = useParams();
   const {
     business: { integrations },
   } = useBusinessContext();
-
-  const handleUpdateJobStatus =
-    (status: Database["public"]["Enums"]["location_job_status"]) =>
-    async () => {
-      await supabase
-        .from("business_location_jobs")
-        .update({ status })
-        .eq("id", Number(jobId));
-
-      router.refresh();
-    };
 
   const tabs = [
     {
@@ -54,60 +38,37 @@ export default function JobHeader({ job }: { job: IJob }) {
   const activePageTabTitle = tabs.find((tab) => tab.href === pathname);
 
   return (
-    <>
-      <header className="space-y-4 text-gray-500 dark:text-gray-300">
-        <Breadcrumb aria-label="Default breadcrumb example">
-          <Breadcrumb.Item
-            href={`/manage/${businessId}/location/${locationId}/jobs`}
-            icon={() => <ChevronLeftIcon className="mr-1" />}
-          >
-            Back to Jobs
-          </Breadcrumb.Item>
-        </Breadcrumb>
-        <div className="flex flex-col items-start justify-between gap-2 sm:flex-row sm:items-center sm:gap-0">
-          <hgroup>
-            <span className="text-3xl font-semibold">{job.full_name}</span>
-            <h1 className="flex items-center gap-1 text-sm text-gray-400">
-              <HardHatIcon className="size-5" />
-              {`Job #${jobId}${activePageTabTitle ? ` ${activePageTabTitle.title}` : ""}`}
-            </h1>
-          </hgroup>
-          <div className="flex flex-wrap gap-2">
-            <Dropdown
-              label={LOCATION_JOB_STATUS[job.status].name}
-              color={LOCATION_JOB_STATUS[job.status].color}
+    <header className="space-y-4 text-gray-500 dark:text-gray-300">
+      <Breadcrumb aria-label="Default breadcrumb example">
+        <Breadcrumb.Item
+          href={`/manage/${businessId}/location/${locationId}/jobs`}
+          icon={() => <ChevronLeftIcon className="mr-1" />}
+        >
+          Back to Jobs
+        </Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="flex flex-col items-start justify-between gap-2 sm:flex-row sm:items-center sm:gap-0">
+        <hgroup>
+          <span className="text-3xl font-semibold">{job.full_name}</span>
+          <h1 className="flex items-center gap-1 text-sm text-gray-400">
+            <HardHatIcon className="size-5" />
+            {`Job #${jobId}${activePageTabTitle ? ` ${activePageTabTitle.title}` : ""}`}
+          </h1>
+        </hgroup>
+      </div>
+      {tabs.length > 1 && (
+        <PageTabs>
+          {tabs.map((tab) => (
+            <PageTabs.Tab
+              key={tab.title}
+              href={tab.href}
+              active={pathname === tab.href}
             >
-              {Object.entries(LOCATION_JOB_STATUS).map(
-                ([locationJobStatusKey, locationJobStatus]) => {
-                  return (
-                    <Dropdown.Item
-                      key={locationJobStatusKey}
-                      onClick={handleUpdateJobStatus(
-                        locationJobStatusKey as Database["public"]["Enums"]["location_job_status"],
-                      )}
-                    >
-                      {locationJobStatus.name}
-                    </Dropdown.Item>
-                  );
-                },
-              )}
-            </Dropdown>
-          </div>
-        </div>
-        {tabs.length > 1 && (
-          <PageTabs>
-            {tabs.map((tab) => (
-              <PageTabs.Tab
-                key={tab.title}
-                href={tab.href}
-                active={pathname === tab.href}
-              >
-                {tab.title}
-              </PageTabs.Tab>
-            ))}
-          </PageTabs>
-        )}
-      </header>
-    </>
+              {tab.title}
+            </PageTabs.Tab>
+          ))}
+        </PageTabs>
+      )}
+    </header>
   );
 }
