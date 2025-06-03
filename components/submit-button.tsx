8938b8d5948e02deb6360337878e356186bf9538
tony <dev@avid-hom.com>
2025-06-03 05:42:12 -0600

    Add form string to SubmitButton

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -5,19 +5,27 @@ import { PropsWithChildren } from "react";
 import { useFormStatus } from "react-dom";
 
 type SubmitButtonProps = PropsWithChildren & {
+  form?: string;
   pendingText?: string;
   size?: "sm" | "md" | "lg";
 };
 
 export default function SubmitButton({
   children,
+  form,
   pendingText,
   size,
 }: SubmitButtonProps) {
   const { pending } = useFormStatus();
 
   return (
-    <Button disabled={pending} type="submit" color="primary" size={size}>
+    <Button
+      color="primary"
+      disabled={pending}
+      form={form}
+      size={size}
+      type="submit"
+    >
       {pending && pendingText ? (
         <>
           <Spinner aria-label="Form submitted" size="sm" className="mr-2" />
